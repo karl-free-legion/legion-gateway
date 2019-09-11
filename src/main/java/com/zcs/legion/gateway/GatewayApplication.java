ddce2d056f6c0d2f255a152894a19185c6599ac1
fan <fjy170@zcsmart.com>
2019-09-11 11:48:29 +0800

    打开跨域请求

--- src/main/java/com/zcs/legion/gateway/GatewayApplication.java
+++ src/main/java/com/zcs/legion/gateway/GatewayApplication.java
@@ -2,6 +2,10 @@ package com.zcs.legion.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * Legion-Gateway
@@ -14,5 +18,17 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+      @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
